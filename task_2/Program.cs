ff77fc7828882ee3cdfec633a994283e82fe5ee5
Hmuriy Monakh <max_08931993@mail.ru>
2022-08-18 16:41:29 +0300

    frozen

---
+++ task_2/Program.cs
@@ -0,0 +1,85 @@
+﻿
+//Задача 55: Задайте двумерный массив. Напишите программу, 
+//которая заменяет строки на столбцы. В случае, если это невозможно, 
+//программа должна вывести сообщение для пользователя.
+
+Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+/*
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+*/
+void PrintMatrix(int[,] matrix)
+{                                  
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 50);
+        }
+    }
+}
+
+void TransP(int[,] matrix, int[,] array2)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array2[i, j] = matrix[i, j];
+        }
+    }
+}
+
+
+FillMatrix(array);
+PrintMatrix(array);
+Console.WriteLine();
+int[,] array2 = new int[n, m];
+try
+{
+    TransP(array, array);
+    PrintMatrix(array2);
+}
+catch
+{
+    Console.WriteLine("Размерность массива должна быть одинаковой");
+} 
+
+
---
+++ task_3/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 57: Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, 
+сколько раз встречается элемент входных данных.
+1, 2, 3 
+4, 6, 1 
+2, 1, 6
+1 встречается 3 раза 
+2 встречается 2 раз 
+3 встречается 1 раз 
+4 встречается 1 раз 
+6 встречается 2 раза*/
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+    for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void ChangeArray(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int t = matr[0,j];
+        matr[0,j]=matr[matr.GetLength(0)-1,j];
+        matr[matr.GetLength(0)-1,j]=t;
+    }
+}
+
+
+var x = new HashSet<int>() { 2, 3, 4 };
+Console.Write("Введите кол-во строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int[m,n];
+FillArray (array);
+Console.WriteLine("Заданный массив: ");
+PrintArray(array);
+ChangeArray(array);
+Console.WriteLine("Образованный массив: ");
+PrintArray(array);
+
+
+
+
+
+
+
